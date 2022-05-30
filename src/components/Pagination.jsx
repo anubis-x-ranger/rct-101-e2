bdd5e3f7c2964ae192ef4cb8a8172f6596a5f4f1
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-30 14:48:11 +0530

    update

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Select } from '@chakra-ui/react'
 
 const Pagination = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const ButtonGroup = () => <div />;
-  const Select = () => <div />;
-
+ 
   return (
-    <ButtonGroup>
-      <Button data-cy="pagination-first-button">First</Button>
-      <Button data-cy="pagination-previous-button">Previous</Button>
-      <Select data-cy="pagination-limit-select">
+    <ButtonGroup size='sm' isAttached variant='outline'>
+      <Button data-cy="pagination-first-button" colorScheme='gray'>First</Button>
+      <Button data-cy="pagination-previous-button" colorScheme='gray'>Previous</Button>
+      <Select data-cy="pagination-limit-select" placeholder='Select option'>
         <option data-cy="pagination-limit-3">3</option>
         <option data-cy="pagination-limit-6">6</option>
         <option data-cy="pagination-limit-9">9</option>
       </Select>
-      <Button data-cy="pagination-next-button">Next</Button>
-      <Button data-cy="pagination-last-button">Last</Button>
+      <Button data-cy="pagination-next-button" colorScheme='gray'>Next</Button>
+      <Button data-cy="pagination-last-button" colorScheme='gray'>Last</Button>
     </ButtonGroup>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product";
+import {Flex,Grid} from '@chakra-ui/react'
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
-
+  
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <AddProduct />
+      <Grid>
+        {/* List of Products */}
+        <Product/>
+      </Grid>
       {/* Pagination */}
+      <Pagination/>
     </Flex>
   );
 };
